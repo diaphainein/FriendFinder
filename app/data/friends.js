75a63d0e349aaacd0dec22a54777e975a9fa3ef7
diaphainein <lauren.cotto@gmail.com>
2017-04-21 23:35:09 -0700

    week 13 homework

---
+++ app/data/friends.js
@@ -0,0 +1,32 @@
+// Friend Finder Data
+var friends = [
+		{
+			"userName": "Lauren", 
+			"userPhoto": "http://vignette1.wikia.nocookie.net/adventuretimewithfinnandjake/images/7/73/LSP%27s_Phone.png/revision/latest?cb=20130724203924",
+			"userScores": [4,3,1,3,5,3,5,5,5,4]
+
+		}, 
+		{
+			"userName": "Aaron", 
+			"userPhoto": "http://krui.fm/wordpress/wp-content/uploads/2016/05/archerbooze.jpg",
+			"userScores": [3,4,2,4,5,2,5,5,5,5]
+		},
+		{
+			"userName": "Sasha",
+			"userPhoto": "https://i.ytimg.com/vi/HdOuXxfnDxo/hqdefault.jpg",
+			"userScores": [2,5,4,2,3,1,3,4,2,5]
+		}, 
+		{
+			"userName": "Jack",
+			"userPhoto": "http://www.citytv.com/wp-content/uploads/2017/03/img-hjonbenjamin-bb.jpg",
+			"userScores": [3,1,2,2,1,5,3,2,2,1]
+		}, 
+		{
+			"userName": "Nina",
+			"userPhoto": "http://img.photobucket.com/albums/v294/MilanoCollectionAT/Draft/714.jpg",
+			"userScores": [1,2,5,2,5,3,2,1,4,4]
+		}
+	];
+
+// exporting our data to be used in other files
+module.exports = friends;
---
+++ server.js
@@ -0,0 +1,27 @@
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// create express server
+var app = express();
+
+// set up initial port
+var PORT = process.env.PORT || 8080;
+
+// the following is so body parser can read the data sent to the server
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+app.use(express.static('app'));
+
+// routing
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+// listener
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
